344171d299475798a68e91d51177725296a6416e
starbot2001 <satejraste@gmail.com>
2025-11-21 18:22:18 +0530

    Error  commit

--- src/train_diabetes.py
+++ src/train_diabetes.py
@@ -39,6 +39,8 @@ def train_and_log(stage: str = "staging") -> None:
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
+    X_train = X_train[:, :-2]
+    X_test = X_test[:, :-2]
 
     # ----------------------------------------
     # 3. Start MLflow run
